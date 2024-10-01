d8854bfc5d767ec674d6f1c445ed1406e68d2b08
Yasser Dhia Hussein <yasser1994hp@yahoo.com>
2024-10-01 06:12:09 +0300

    move profile to path pages/profile

--- login-register-app/frontend/src/app/app.routes.ts
+++ login-register-app/frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { ProfileComponent } from './profile/profile.component'; // استيراد مكون صفحة البروفايل
+import { ProfileComponent } from './pages/profile/profile.component'; // استيراد مكون صفحة البروفايل
 import { AuthGuard } from './guards/auth.guard'; // استيراد الحارس من المجلد الجديد
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component'; // استيراد مكون "نسيت كلمة المرور"
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component'; // استيراد مكون إعادة تعيين كلمة المرور
